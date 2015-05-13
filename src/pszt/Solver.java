b2087d4d5e4d01055177a786d61b3f3e640e7a94
Radosław Białobrzeski <radoslawbialobrzeski@gmail.com>
2015-05-13 18:48:19 +0200

    Modyfikacja czyMerge
    Kolejna drobna poprawka. Tym razem czyMerge - zastosowałem logikę
    trójwartościową i teraz wartość 2 jest zwracana, kiedy możemy dokonać
    podstawienia. Gdzie możemy sobie dokonać tego podstawienia to już sobie
    funkcja podstawiająca sama ogarnie.

--- src/pszt/Solver.java
+++ src/pszt/Solver.java
@@ -7,22 +7,32 @@ public class Solver {
 		Klauzula teza;
 		BazaWiedzy bazaWiedzy;
 		ArrayList<Klauzula> mikroBaza = new ArrayList<Klauzula>();
-		void rezolucja(){
+		
+		//zmiana typu zwracanego na int:
+		//0 - nie da sie
+		//1 - da sie
+		//2 - podstawionko
+		int czyMerge(Klauzula K1, Klauzula K2){
+			int check = 0, checkPodstawienie = 0;
 			
-		}
-		boolean czyMerge(Klauzula K1, Klauzula K2){
-			int check = 0;
 			for (Literal literal1: K1.getLiteraly()){
 				for(Literal literal2: K2.getLiteraly()){
-					if(literal1.getNazwa().equals((literal2).getNazwa()) && literal1.getNegacja()!=literal2.getNegacja()){
-						check = check + 1;
+					if(literal1.getNazwa().equals((literal2).getNazwa()) && literal1.getNegacja()!=literal2.getNegacja())
+					{
+						if(!Literal.sprawdzArgumenty(literal1, literal2))
+						{
+							checkPodstawienie++;
+							continue;
+						}
+						check++;
 					}
 				}
 			}
 			Klauzula klauzula = new Klauzula(K1, K2);
-			if(bazaWiedzy.czyIstnieje(klauzula))return false;
-			if (check==1)return true;
-			return false;
+			if(bazaWiedzy.czyIstnieje(klauzula)) return 0;
+			if (check==1)return 1;
+			else if(checkPodstawienie!=0) return 2;
+			return 0;
 		}
 	public
 		Solver(BazaWiedzy bazaWiedzy){
@@ -38,7 +48,7 @@ public class Solver {
 			while(!bazaWiedzy.sprawdzSprzecznosc()){
 				for (Klauzula klauzula1: bazaWiedzy.getBaza()){
 					for (Klauzula klauzula2: bazaWiedzy.getBaza()){
-						if(czyMerge(klauzula1, klauzula2)){
+						if(czyMerge(klauzula1, klauzula2) == 1){
 							Klauzula klauzula = new Klauzula(klauzula1, klauzula2);
 							mikroBaza.add(klauzula);
 							log.dodajKlauzule(klauzula1, klauzula2, klauzula, i);
@@ -73,7 +83,7 @@ public class Solver {
 			boolean stop = false;
 			while(!bazaWiedzy.sprawdzSprzecznosc()){
 				for (Klauzula klauzula1: bazaWiedzy.getBaza()){
-						if(czyMerge(klauzula1, aktualna)){
+						if(czyMerge(klauzula1, aktualna) == 1){
 							Klauzula klauzula = new Klauzula(klauzula1, aktualna);
 							mikroBaza.add(klauzula);
 							log.dodajKlauzule(klauzula1, aktualna, klauzula, i);
